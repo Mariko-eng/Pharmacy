045803955f0ee13092534b33dfefc60cbef0ecc1
Mariko-eng <yigamarkgabriel333@gmail.com>
2024-04-17 16:17:29 +0300

    File Path Fix

--- pharmacy/utils.py
+++ pharmacy/utils.py
@@ -5,32 +5,37 @@ def generate_random_number():
     otp = random.randint(100000, 999999)
     return str(otp)
 
-def build_file_path(request, file_url = None):
-    print(f"file_url : {file_url}")
 
+def build_file_path(request, file_url = None):
     # Get the request's host
     host = request.get_host()
-    print(f"host : {host}")
+
+    print(f"host1 : {host}")
 
     # Check if the host is an IP address
     if host.replace('.', '').isdigit():  # Checking if host is an IP address
         # Get the port number from the request
-        port = request.META.get('SERVER_PORT', '')
+        host = request.META.get('REMOTE_ADDR', '127.0.0.1')
+        port = request.META.get('REMOTE_PORT', '')
         if port:
             # Append the port number to the host
             host += f':{port}'
 
-    print(f"host2 : {host}")
-    
-    invoice_url = request.build_absolute_uri(file_url) if file_url else None
+        print(f"host2 : {host}")
+        
+        invoice_url = request.build_absolute_uri(file_url) if file_url else None
+        
+        if invoice_url:
+        # Replace the hostname with the updated one
+            parsed_url = urlparse(invoice_url)
+            invoice_url = parsed_url._replace(netloc=host).geturl()
 
-    print(f"invoice_url : {invoice_url}")
+        print(f"invoice_url1 : {invoice_url}")
+        return invoice_url
+    else:
+        invoice_url = request.build_absolute_uri(file_url) if file_url else None
 
-    if invoice_url:
-    # Replace the hostname with the updated one
-        parsed_url = urlparse(invoice_url)
-        invoice_url = parsed_url._replace(netloc=host).geturl()
-
-    return invoice_url
+        print(f"invoice_url2 : {invoice_url}")
+        return invoice_url
 
 
